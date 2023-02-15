df025dbbb9f4a7bc2227945dbe52a2b584ec2ac7
Vitor Borgatte <vitor.borgatte@icloud.com>
2023-02-15 18:41:45 -0300

    projeto finalizado

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 import chalk from 'chalk';
 import readline from 'readline-sync';
 
-// adicionar mais cores
-const red = chalk.red;
+const red = chalk.redBright;
+const magenta = chalk.magenta;
+const cyan = chalk.cyanBright;
+const yellow = chalk.yellowBright;
 let props = [];
 let input = '';
 
 while (input != 'sair') {
   input = readline
     .question(
-      'Insira um propriedade CSS ou digite "sair" para encerrar o programa: \n'
+      magenta(
+        'Insira um propriedade CSS ou digite "sair" para encerrar o programa: \n'
+      )
     )
     .toLowerCase();
 
   if (input === 'sair') {
-    console.log(`\n ${props.sort().join('\n')}`);
+    console.log(
+      yellow(
+        'A lista de propriedades CSS, organizada alfabeticamente, ficou assim: \n'
+      ),
+      cyan(`\n${props.sort().join('\n')}`)
+    );
   }
 
   if (input != '' && !props.includes(input)) {
